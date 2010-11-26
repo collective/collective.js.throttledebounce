82a331e5183e641d2519f2ca11619ed9b6bbfec8
Jonas Baumann <jone@jone.ch>
2010-11-26 11:37:14 +0100

    prepare for moving to github

--- README.txt
+++ README.txt
@@ -2,3 +2,5 @@ Introduction
 ============
 
 jQuery throttle / debounce allows you to rate-limit your functions in multiple useful ways.
+
+See: http://benalman.com/projects/jquery-throttle-debounce-plugin/
--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('collective/js/throttledebounce/version.txt').read().strip()
-maintainer = 'Jonas Baumann'
+version = '1.1-dev'
 
 setup(name='collective.js.throttledebounce',
       version=version,
       description="Installs the throttle / debounce plugin",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         ],
       keywords='',
-      author='%s, 4teamwork GmbH',
+      author='Jonas Baumann, 4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
-      maintainer=maintainer,
-      url='http://psc.4teamwork.ch/dist/collective-js-throttledebounce',
+      url='https://github.com/collective/collective.js.throttledebounce',
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.js'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
